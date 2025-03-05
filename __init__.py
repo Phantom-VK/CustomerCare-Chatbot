a39be7822e74966edc73de2972c2e69f763f7a64
Phantom-VK <vikramadityakhupse@gmail.com>
2025-03-05 11:40:26 +0530

    renamed main.py to model.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-from main import model, collection
+from model import model, collection
 
-while True:
-    query = input("\nAsk a question: ")
-    query_embedding = model.encode(query).tolist()
-    results = collection.query(
-        query_embeddings=[query_embedding],
-        n_results=1
-    )
-    print("Answer:", results["documents"][0][0])
+
+if __name__ == "__main__":
+    while True:
+        query = input("\nAsk a question: ")
+        query_embedding = model.encode(query).tolist()
+        results = collection.query(
+            query_embeddings=[query_embedding],
+            n_results=1
+        )
+        print("Answer:", results["documents"][0][0])
